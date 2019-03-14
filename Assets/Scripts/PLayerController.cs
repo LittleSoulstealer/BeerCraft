982e9c28f0afc3efdbd70973e351b5cc11075d12
LittleSoulstealer <alkemiczka@gmail.com>
2019-03-14 23:04:52 +0100

    Initial commit

---
+++ Assets/Scripts/PLayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PLayerController : MonoBehaviour
+  
+{
+
+    public float moveSpeed;
+    private Animator anim;
+    bool playerMoving;
+    Vector2 lastMove;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Moving();
+    }
+
+    void Moving()
+    {
+        playerMoving = false;
+      
+        if (Input.GetAxisRaw("Horizontal") != 0)
+        {
+            playerMoving = true;
+            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+            lastMove = new Vector2(Input.GetAxisRaw("Horizontal"),0f);
+
+        }
+
+       if (Input.GetAxisRaw("Vertical") != 0)
+        {
+            playerMoving = true;
+            transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+            lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
+
+        }
+        anim.SetFloat("moveX", Input.GetAxisRaw("Horizontal"));
+        anim.SetFloat("moveY", Input.GetAxisRaw("Vertical"));
+        anim.SetBool("playerMoving", playerMoving);
+        anim.SetFloat("lastMoveX", lastMove.x);
+        anim.SetFloat("lastMoveY", lastMove.y);
+    }
+}
