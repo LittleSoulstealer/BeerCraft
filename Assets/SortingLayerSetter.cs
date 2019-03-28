3b3aed006090c2dfd2b96fc378708f571a5e3916
LittleSoulstealer <alkemiczka@gmail.com>
2019-03-28 22:33:02 +0100

    Sorting layer order

---
+++ Assets/SortingLayerSetter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class SortingLayerSetter : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+    public bool updateEveryFrame = false;
+    
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        SetSortingOrder();
+    }
+
+    void Update()
+    {
+        if(updateEveryFrame)
+            SetSortingOrder();
+    }
+
+    private void SetSortingOrder()
+    {
+        spriteRenderer.sortingOrder = GetSortingOrder();
+    }
+
+    private int GetSortingOrder()
+    {
+        return GetBaseHeightInCm();
+    }
+
+    private int GetBaseHeightInCm()
+    {
+        return -(int)transform.position.y*100;
+    }
+}
