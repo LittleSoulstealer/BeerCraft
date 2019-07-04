2ebc9672daa7ba5994757363d4c00b20c08403a4
LittleSoulstealer <alkemiczka@gmail.com>
2019-07-04 20:03:25 +0200

    Almost working shop.

--- Assets/Shop.cs
+++ Assets/Shop.cs
@@ -1,39 +1,89 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 public class Shop : MonoBehaviour
 {
     List<InventoryItem> shopItemList;
-    int[] shopPrices;
     List<InventoryItem> playerItemList;
-    int money;
+    [SerializeField] Button shopButton;
+    [SerializeField] GameObject shopItems;
+    [SerializeField] GameObject playerItems;
+    [SerializeField] GameObject shopUI;
+
+    [SerializeField] InventoryItem bottles;
+    [SerializeField] InventoryItem seeds;
+    List<Button> shopButtons = new List<Button>();
     // Start is called before the first frame update
 
-     
+
     void Start()
     {
-       
-        shopItemList = new List<InventoryItem>
-        {
-         
-        };
+
+        shopItemList = new List<InventoryItem>();
+
+        playerItemList = new List<InventoryItem>();
+        SetShop();
+        shopUI.SetActive(false);
 
     }
-
-    void EnterShop()
+    
+    void SetShop()
     {
-        playerItemList = new List<InventoryItem>();
-
-
-        foreach (InventoryItem item in Inventory.instance.items)
+        bottles = Instantiate(bottles, transform);
+        seeds = Instantiate(seeds, transform);
+        shopItemList.Add(bottles);
+        shopItemList.Add(seeds);
+        foreach (InventoryItem item in shopItemList)
         {
-            if (item.amount > 0 && !shopItemList.Contains(item))
+
+            Button item1 = Instantiate(shopButton, shopItems.transform);
+
+
+            Text[] namePrice = item1.GetComponentsInChildren<Text>();
+            namePrice[0].text = item.name;
+            namePrice[1].text = item.price.ToString() + " G";
+            shopButtons.Add(item1);
+
+        }
+
+    }
+    private void Update()
+    {
+        if (shopUI.activeInHierarchy && Input.GetKeyDown(KeyCode.Escape))
+            { 
+            shopUI.SetActive(false);
+        }
+    }
+
+    public void EnterShop()
+    {
+        shopUI.SetActive(true);
+        playerItemList.Clear();
+        foreach (Transform child in playerItems.transform)
+        {
+            GameObject.Destroy(child.gameObject);
+        }
+
+        foreach (InventoryItem item in Inventory.instance.items )
+        {
+            if (item.amount > 0 && !shopItemList.Contains(item as InventoryItem))
             {
                 playerItemList.Add(item);
             }
 
+        }
+        foreach(InventoryItem item in playerItemList)
+        {
+            Button item1 = Instantiate(shopButton, playerItems.transform);
+
+
+            Text[] namePrice = item1.GetComponentsInChildren<Text>();
+            namePrice[0].text = item.name;
+            namePrice[1].text = item.price.ToString() + " G";
+            shopButtons.Add(item1);
         }
     }
 
@@ -48,6 +98,6 @@ public class Shop : MonoBehaviour
     void Sell(InventoryItem item)
     {
         Inventory.instance.money += item.price;
-        Inventory.instance.items.Remove(item);
+        Inventory.instance.Remove(item,1);
     }
 }
