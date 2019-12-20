3f6b831d259ae3d24643b0666d6e8a423241d4dd
Marta Gejdel <marta.gejdel@microsoft.wsei.edu.pl>
2019-12-20 13:12:17 +0100

    Event

---
+++ Assets/EventSpawnEnemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventSpawnEnemy : MonoBehaviour
+{
+    public GameObject spawnPoint;
+    [SerializeField] GameObject player;
+    public GameObject prefabToSpawn;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject==player)
+        {
+            GameObject enemy = Instantiate(prefabToSpawn);
+            enemy.transform.position = spawnPoint.transform.position;
+            gameObject.SetActive(false);
+        }
+    }
+
+}
